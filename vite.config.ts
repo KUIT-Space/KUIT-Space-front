14717c847a6f04d97753b403f7b195ebb85f4bcb
kim jihwan <kjhwan0802@naver.com>
2024-06-29 15:15:32 +0900

    vite + React + TS / eslint, prettier initial  setting finish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import * as path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import vitePluginSvgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePluginSvgr()],
+  resolve: {
+    alias: {
+      "@" : path.resolve(__dirname, "./src"),
+    }
+  }
 })
