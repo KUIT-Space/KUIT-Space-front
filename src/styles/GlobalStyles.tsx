5976d4fad05bac6fe15799581231531303c536af
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-09 14:51:57 -0700

    Add GlobalStyles.tsx
    GlobalStyles.tsx의 추가 및 배경색 지정

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    :root {
+        background-color:#171719;
+    }
+`
+
+export default GlobalStyle;
