23cf8423c88efc2086fbbbc44c35a9806b96c7fa
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-09 14:52:39 -0700

    GlobalStyles의 추가에 따른 main.tsx 변경
    main에 GlobalStyles를 추가하여 해당 내용 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import GlobalStyle from './styles/GlobalStyles.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyle/>
     <App />
   </React.StrictMode>,
 )
