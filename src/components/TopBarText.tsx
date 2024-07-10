e55d026c7a7039523f8a093aee7e80a99de76087
Kim jihwan <kjhwan0802@naver.com>
2024-07-10 19:25:52 +0900

    style: TopBarText import 절대경로, styled-component 분리

--- src/components/TopBarText.tsx
+++ src/components/TopBarText.tsx
@@ -1,82 +1,80 @@
-import { FC } from "react"
-import logo from "../assets/logo_space.svg"
-import back from "../assets/icon_back.svg"
-import * as sty from "../styles/TopBarTextStyles"
-import '../index.css'
+import { FC } from "react";
+import logo from "@/assets/logo_space.svg";
+import back from "@/assets/icon_back.svg";
+import * as sty from "@/styles/TopBarText.styled";
 import { useNavigate } from "react-router-dom";
 
-
-
-
 export enum LeftEnum {
-    Logo = "logo",
-    Back = "back",
-    None = "none"
-};
+	Logo = "logo",
+	Back = "back",
+	None = "none",
+}
 
 interface topbarProps {
-    left: LeftEnum;
-    center: string;
-    right: string;
+	left: LeftEnum;
+	center: string;
+	right: string;
 }
 
 const TopBarText: FC<topbarProps> = ({ left, center, right }) => {
-    const navigate = useNavigate();
-    switch (left) {
-        case "logo":
-            return (
-                <div>
-                    <sty.StyledTopBarDiv>
-                        <nav>
-                            <sty.StyledLeftDiv onClick={() => { navigate('/') }} style={{ cursor: "pointer" }}>
-                                <img src={logo} width="120px"></img>
-                            </sty.StyledLeftDiv>
-                        </nav>
-                        <nav>
-                            <sty.StyledCenterDiv>
-                                <sty.StyledCenterP>{center}</sty.StyledCenterP>
-                            </sty.StyledCenterDiv>
-                        </nav>
-                        <nav>
-                            <sty.StyledRightDiv>
-                                <sty.StyledRightP>{right}</sty.StyledRightP>
-                            </sty.StyledRightDiv>
-                        </nav>
-                    </sty.StyledTopBarDiv>
-                </div >
-            );
-        case "back":
-            return (
-                <div>
-                    <sty.StyledTopBarDiv>
-                        <sty.StyledLeftDiv onClick={() => { navigate(-1) }} style={{ cursor: "pointer" }} >
-                            <img src={back} width="120px"></img>
-                        </sty.StyledLeftDiv>
-                        <sty.StyledCenterDiv>
-                            <sty.StyledCenterP>{center}</sty.StyledCenterP>
-                        </sty.StyledCenterDiv>
-                        <sty.StyledRightDiv>
-                            <sty.StyledRightP>{right}</sty.StyledRightP>
-                        </sty.StyledRightDiv>
-                    </sty.StyledTopBarDiv>
-                </div>
-            );
-        case "none":
-            return (
-                <div>
-                    <sty.StyledTopBarDiv>
-                        <sty.StyledLeftDiv>
-                        </sty.StyledLeftDiv>
-                        <sty.StyledCenterDiv>
-                            <sty.StyledCenterP>{center}</sty.StyledCenterP>
-                        </sty.StyledCenterDiv>
-                        <sty.StyledRightDiv>
-                            <sty.StyledRightP>{right}</sty.StyledRightP>
-                        </sty.StyledRightDiv>
-                    </sty.StyledTopBarDiv>
-                </div>
-            )
-    }
+	const navigate = useNavigate();
+	switch (left) {
+		case "logo":
+			return (
+				<div>
+					<sty.StyledTopBarDiv>
+						<sty.StyledLeftDiv
+							onClick={() => {
+								navigate("/");
+							}}
+						>
+							<img src={logo}></img>
+						</sty.StyledLeftDiv>
+						<sty.StyledCenterDiv>
+							<sty.StyledCenterP>{center}</sty.StyledCenterP>
+						</sty.StyledCenterDiv>
+						<sty.StyledRightDiv>
+							<sty.StyledRightP>{right}</sty.StyledRightP>
+						</sty.StyledRightDiv>
+					</sty.StyledTopBarDiv>
+				</div>
+			);
 
+		case "back":
+			return (
+				<div>
+					<sty.StyledTopBarDiv>
+						<sty.StyledLeftDiv
+							onClick={() => {
+								navigate(-1);
+							}}
+						>
+							<img src={back}></img>
+						</sty.StyledLeftDiv>
+						<sty.StyledCenterDiv>
+							<sty.StyledCenterP>{center}</sty.StyledCenterP>
+						</sty.StyledCenterDiv>
+						<sty.StyledRightDiv>
+							<sty.StyledRightP>{right}</sty.StyledRightP>
+						</sty.StyledRightDiv>
+					</sty.StyledTopBarDiv>
+				</div>
+			);
+
+		case "none":
+			return (
+				<div>
+					<sty.StyledTopBarDiv>
+						<sty.StyledLeftDiv></sty.StyledLeftDiv>
+						<sty.StyledCenterDiv>
+							<sty.StyledCenterP>{center}</sty.StyledCenterP>
+						</sty.StyledCenterDiv>
+						<sty.StyledRightDiv>
+							<sty.StyledRightP>{right}</sty.StyledRightP>
+						</sty.StyledRightDiv>
+					</sty.StyledTopBarDiv>
+				</div>
+			);
+	}
 };
 export default TopBarText;
