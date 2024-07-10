0930166effe6af692c50cf174a40ea150e409a10
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-10 06:16:54 -0700

    Update BottomNavBar.tsx
    No router
    Just design file

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,6 +1,34 @@
+import styled from "styled-components"
+
+import board from "@/assets/icon_board.svg"
+import chat from "@/assets/icon_chat.svg"
+import home from "@/assets/icon_home.svg"
+import pay from "@/assets/icon_pay.svg"
+import voice from "@/assets/icon_voice.svg"
+
+const StyledDiv = styled.div`
+  width: 640px;
+  padding: 10px;
+  display: flex;
+  justify-content: space-evenly;
+  background-color: #222226;
+`
+
+const MenuDiv = styled.div`
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 const BottomNavBar = () => {
   return (
-    <div>BottomNavBar</div>
+    <StyledDiv>
+      <MenuDiv><img src={voice} />보이스룸</MenuDiv>
+      <MenuDiv><img src={chat} />채팅</MenuDiv>
+      <MenuDiv><img src={home} />홈</MenuDiv>
+      <MenuDiv><img src={pay} />정산</MenuDiv>
+      <MenuDiv><img src={board} />게시판</MenuDiv>
+    </StyledDiv>
   )
 }
 
