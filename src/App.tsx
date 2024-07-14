faa4f34ab3f8d61874121ff731dbe10d90f6d6c7
woo319 <132249641+woo319@users.noreply.github.com>
2024-07-14 20:55:20 +0900

    Feat#18 login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import BottomNavBar from "@/components/BottomNavBar";
 import LoginPage from "@/pages/LoginPage/LoginPage";
+import SignUpPage from "@/pages/LoginPage/SignUpPage";
 import HomePage from "@/pages/HomePage";
 import VoiceRoomListPage from "@/pages/VoiceRoomPage/VoiceRoomListPage";
 
@@ -15,11 +16,12 @@ function Layout() {
 
 function App() {
 	const routes = [
+		{ path: "/login", element: <LoginPage /> },
+		{ path: "/signUp", element: <SignUpPage /> },
 		{
 			element: <Layout />,
 			children: [
 				{ path: "/", element: <HomePage /> },
-				{ path: "/login", element: <LoginPage /> },
 				{ path: "/voiceroom", element: <VoiceRoomListPage /> },
 			],
 		},
---
+++ src/pages/LoginPage/SignUpPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div>
+      <h1>회원가입 페이지~~</h1>
+    </div>
+  );
+}
+
+export default SignUp;
