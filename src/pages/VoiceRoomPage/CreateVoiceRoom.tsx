ab38d3510d9114a72093fdf1dfda76b3bbe34837
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-15 22:07:55 -0700

    a

--- src/pages/VoiceRoomPage/CreateVoiceRoom.tsx
+++ src/pages/VoiceRoomPage/CreateVoiceRoom.tsx
@@ -3,12 +3,14 @@ import { LeftEnum } from "@/components/TopBarText";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as s from "@/pages/VoiceRoomPage/CreateVoiceRoomPage.styled";
+import styled from "styled-components";
 
+const VRName = styled.p``;
 const CreateVoiceRoomPage = () => {
 	const navigate = useNavigate();
 	return (
 		<div>
-			<p></p>
+			<TopBarText left={LeftEnum.Back} center="새 보이스룸" right="이것은 우측" />
 		</div>
 	);
 };
