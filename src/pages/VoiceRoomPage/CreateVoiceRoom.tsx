4d85749b3d7674be1806a440f2d4fec1942c6dad
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-15 19:57:55 -0700

    add CreateVoiceRoom Files

---
+++ src/pages/VoiceRoomPage/CreateVoiceRoom.tsx
@@ -0,0 +1,16 @@
+import TopBarText from "@/components/TopBarText";
+import { LeftEnum } from "@/components/TopBarText";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as s from "@/pages/VoiceRoomPage/CreateVoiceRoomPage.styled";
+
+const CreateVoiceRoomPage = () => {
+	const navigate = useNavigate();
+	return (
+		<div>
+			<p></p>
+		</div>
+	);
+};
+
+export default CreateVoiceRoomPage;
