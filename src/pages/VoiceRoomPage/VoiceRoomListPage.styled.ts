fdd31d8953015e0900df11c982c19cd8df1feea4
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-15 19:57:41 -0700

    Update VoiceRoomListPage.styled.ts
    px change to "rem and %"

--- src/pages/VoiceRoomPage/VoiceRoomListPage.styled.ts
+++ src/pages/VoiceRoomPage/VoiceRoomListPage.styled.ts
@@ -5,14 +5,14 @@ import purple from "@/assets/VoiceRoom/purple_gradient.svg";
 
 export const ActiveP = styled.p`
 	font-family: "Freesentation SB";
-	font-size: 18px;
+	font-size: 1.125rem;
 `;
 export const VRTitleP = styled.p`
 	font-family: "Freesentation SB";
-	font-size: 20px;
+	font-size: 1.25rem;
 	color: black;
 	position: absolute;
-	margin-left: 16px;
+	margin-left: 1rem;
 `;
 export const BGdiv = styled.div`
 	display: flex;
@@ -20,7 +20,7 @@ export const BGdiv = styled.div`
 	background-image: url(${yellow});
 	background-repeat: no-repeat;
 	background-size: cover;
-	width: 640px;
+	width: 100%;
 	height: 248px;
 `;
 export const BGdiv2 = styled.div`
@@ -29,14 +29,14 @@ export const BGdiv2 = styled.div`
 	background-image: url(${purple});
 	background-repeat: no-repeat;
 	background-size: cover;
-	width: 640px;
+	width: 100%;
 	height: 248px;
 `;
 export const RoundDiv = styled.div`
 	background-color: #222226;
-	border-radius: 20px;
-	padding: 5px 10px 5px 10px;
-	font-size: 14px;
+	border-radius: 1.25rem;
+	padding: 0.25rem 0.5rem 0.25rem 0.5rem;
+	font-size: 0.875rem;
 	font-family: "Freesentation M";
 `;
 
@@ -44,12 +44,11 @@ export const StyledButton = styled.button`
 	background-color: #171719;
 	border-radius: 12px;
 	border: 1px solid #767681;
-	padding: 20px 0px 20px 0px;
-	margin-top: 10px;
-	margin-bottom: 20px;
+	padding: 1rem 0px 1rem 0px;
+	margin: 0.875rem 0rem 1.25rem 0rem;
 	color: #d4d4d9;
 
-	width: 640px;
-	font-size: 24px;
+	width: 100%;
+	font-size: 1.5rem;
 	font-family: "Freesentation M";
 `;
