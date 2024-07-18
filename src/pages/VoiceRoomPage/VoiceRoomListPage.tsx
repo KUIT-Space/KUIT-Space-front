bb1f6309a3b9f05c2ca33bae07d26ce3de0886d2
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-18 15:21:26 -0700

    20240718 1521  update file
    CreateVoiceRoom.tsx
    VoiceRoomListPage.tsx

--- src/pages/VoiceRoomPage/VoiceRoomListPage.tsx
+++ src/pages/VoiceRoomPage/VoiceRoomListPage.tsx
@@ -8,13 +8,12 @@ import * as s from "@/pages/VoiceRoomPage/VoiceRoomListPage.styled";
 function memberList() {}
 
 function voiceRoomList() {
-    const [arr, setArr] = useState([]);
-    const checkHandler = () => {
-        //fetch list
-    }
+	const [arr, setArr] = useState([]);
+	const checkHandler = () => {
+		//fetch list
+	};
 }
-<<<<<<< Updated upstream
-=======
+
 const VoiceRoomListPage = () => {
 	const navigate = useNavigate();
 	return (
@@ -40,52 +39,6 @@ const VoiceRoomListPage = () => {
 			<s.ActiveP> 아무도 없어요! </s.ActiveP>
 			<s.RoundDiv2>보이스룸 3</s.RoundDiv2>
 			<s.RoundDiv2>보이스룸 4</s.RoundDiv2>
-			<div>
-				<s.StyledButton>
-					<div
-						style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-						onClick={() => {
-							navigate("/createvoiceroom");
-						}}
-					>
-						<img src={plus} style={{ marginRight: "8px" }} />
-						새로 만들기
-					</div>
-				</s.StyledButton>
-			</div>
-		</div>
-	);
-};
->>>>>>> Stashed changes
-
-function memberList() {
-
-}
-const VoiceRoomListPage = () => {
-	const navigate = useNavigate();
-	return (
-		<div style={{ width: "100%", margin: "auto" }}>
-			<TopBarText left={LeftEnum.Logo} center="보이스룸" right="" />
-			<s.ActiveP> 활동 중인 보이스룸 </s.ActiveP>
-			<s.BGdiv>
-				<div>
-					<s.VRTitleP> {"보이스룸 1"} </s.VRTitleP>
-					<div style={{ display: "flex", alignItems: "center", width: "640px", height: "124px", marginLeft: "16px" }}>
-						<s.RoundDiv>대화 중인 스페이서 6명</s.RoundDiv>
-					</div>
-				</div>
-			</s.BGdiv>
-			<s.BGdiv2 style={{ marginTop: "12px" }}>
-				<div>
-					<s.VRTitleP> {"보이스룸 1"} </s.VRTitleP>
-					<div style={{ display: "flex", alignItems: "center", width: "640px", height: "124px", marginLeft: "16px" }}>
-						<s.RoundDiv>대화 중인 스페이서 6명</s.RoundDiv>
-					</div>
-				</div>
-			</s.BGdiv2>
-			<s.ActiveP> 아무도 없어요! </s.ActiveP>
-			<s.RoundDiv style={{ color: "#767681", paddingTop: "20px", paddingBottom: "20px", marginBottom: "10px" }}>보이스룸 3</s.RoundDiv>
-			<s.RoundDiv style={{ color: "#767681", paddingTop: "20px", paddingBottom: "20px", marginBottom: "10px" }}>보이스룸 4</s.RoundDiv>
 			<div>
 				<s.StyledButton>
 					<div
