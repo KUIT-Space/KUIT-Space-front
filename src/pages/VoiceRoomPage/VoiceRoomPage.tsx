6a70c1e853aac38787bc21fe07749558f7a33943
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-21 14:31:11 -0700

    Create VoiceRoomPage.tsx

---
+++ src/pages/VoiceRoomPage/VoiceRoomPage.tsx
@@ -0,0 +1,12 @@
+import TopBarText from "@/components/TopBarText";
+import { LeftEnum } from "@/components/TopBarText";
+import plus from "@/assets/VoiceRoom/icon_plus.svg";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as s from "@/pages/VoiceRoomPage/VoiceRoomListPage.styled";
+
+const VoiceRoomPage = () => {
+	return <div></div>;
+};
+
+export default VoiceRoomPage;
