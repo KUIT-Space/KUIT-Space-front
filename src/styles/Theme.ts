ccf8903e5de32277ffa7f21c008df499ed7f6446
Kim jihwan <kjhwan0802@naver.com>
2024-07-21 19:02:35 +0900

    bug: fix wrong single quote in media query

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,9 +1,9 @@
 import { DefaultTheme } from "styled-components";
 
 const windowSize = {
-	small: "screen and (max-width: `600px`)",
-	base: "screen and (max-width: `768px`)",
-	large: "screen and (max-width: `1024px`)",
+	small: "screen and (max-width: 450px)",
+	base: "screen and (max-width: 768px)",
+	large: "screen and (max-width: 1024px)",
 };
 
 const fontSize = {
