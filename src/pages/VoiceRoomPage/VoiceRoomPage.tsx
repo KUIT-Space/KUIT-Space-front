c6063888eb3d2ab9bcd54903870f925d6cda8cd6
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-21 14:37:53 -0700

    Update VoiceRoomPage.tsx

--- src/pages/VoiceRoomPage/VoiceRoomPage.tsx
+++ src/pages/VoiceRoomPage/VoiceRoomPage.tsx
@@ -1,12 +1,35 @@
 import TopBarText from "@/components/TopBarText";
 import { LeftEnum } from "@/components/TopBarText";
 import plus from "@/assets/VoiceRoom/icon_plus.svg";
+import back from "@/assets/icon_back.svg";
+import setting from "@/assets/VoiceRoom/icon_setting.svg";
+
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as s from "@/pages/VoiceRoomPage/VoiceRoomListPage.styled";
+import * as sty from "@/components/TopBarText.styled";
 
-const VoiceRoomPage = () => {
-	return <div></div>;
+const VoiceRoomPage = ({ VoiceRoomName }: { VoiceRoomName: string }) => {
+	const navigate = useNavigate();
+	return (
+		<div>
+			<sty.StyledTopBarDiv>
+				<sty.StyledLeftDiv
+					onClick={() => {
+						navigate(-1);
+					}}
+				>
+					<img src={back}></img>
+				</sty.StyledLeftDiv>
+				<sty.StyledCenterDiv>
+					<sty.StyledCenterP>{VoiceRoomName}</sty.StyledCenterP>
+				</sty.StyledCenterDiv>
+				<sty.StyledRightDiv>
+					<img src={setting} />
+				</sty.StyledRightDiv>
+			</sty.StyledTopBarDiv>
+		</div>
+	);
 };
 
 export default VoiceRoomPage;
