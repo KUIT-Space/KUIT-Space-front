4c08e498ff69284a4ae03dcc0ef1e81d5d6b5018
Kim jihwan <kjhwan0802@naver.com>
2024-07-21 19:03:08 +0900

    style: sticky BottomNavBar that always on bottom

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,37 +1,53 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import board from "@/assets/icon_board.svg";
+import chat from "@/assets/icon_chat.svg";
+import home from "@/assets/icon_home.svg";
+import pay from "@/assets/icon_pay.svg";
+import voice from "@/assets/icon_voice.svg";
+import { Link } from "react-router-dom";
 
-import board from "@/assets/icon_board.svg"
-import chat from "@/assets/icon_chat.svg"
-import home from "@/assets/icon_home.svg"
-import pay from "@/assets/icon_pay.svg"
-import voice from "@/assets/icon_voice.svg"
+const MenuContainer = styled.div`
+	height: 3.75rem;
+	display: flex;
+	justify-content: space-evenly;
+	background-color: ${({ theme }) => theme.colors.BG800};
+	position: sticky;
+	bottom: 0;
+`;
 
-const StyledDiv = styled.div`
-  width: 640px;
-  padding: 10px;
-  display: flex;
-  justify-content: space-evenly;
-  background-color: #222226;
-`
+const Menu = styled(Link)`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 3rem;
+	font-size: 0.875rem;
+`;
 
-const MenuDiv = styled.div`
-  display:flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`
 const BottomNavBar = () => {
-  return (
-    <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
-      <StyledDiv>
-        <MenuDiv><img src={voice} />보이스룸</MenuDiv>
-        <MenuDiv><img src={chat} />채팅</MenuDiv>
-        <MenuDiv><img src={home} />홈</MenuDiv>
-        <MenuDiv><img src={pay} />정산</MenuDiv>
-        <MenuDiv><img src={board} />게시판</MenuDiv>
-      </StyledDiv>
-    </div>
-  )
-}
+	return (
+		<MenuContainer>
+			<Menu to="/voiceroom">
+				<img src={voice} />
+				보이스룸
+			</Menu>
+			<Menu to="/chat">
+				<img src={chat} />
+				채팅
+			</Menu>
+			<Menu to="/">
+				<img src={home} />홈
+			</Menu>
+			<Menu to="/voiceroom">
+				<img src={pay} />
+				정산
+			</Menu>
+			<Menu to="/voiceroom">
+				<img src={board} />
+				게시판
+			</Menu>
+		</MenuContainer>
+	);
+};
 
-export default BottomNavBar
+export default BottomNavBar;
