532f2540ae254e2d8017961708244cb3e85c8f14
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-28 12:38:10 -0700

    feat : FlexDiv , ImgDiv 추가

--- src/pages/PayPage/PayPage.styled.ts
+++ src/pages/PayPage/PayPage.styled.ts
@@ -11,16 +11,16 @@ export const RoundDiv = styled.div`
 	margin: 0.75rem;
 	padding: 0.75rem;
 
-	border-radius: 12px;
+	border-radius: 0.75rem;
 	background: var(--Foundation-Gray-gray800, #222226);
 `;
 
 export const ContentDiv = styled.div`
 	display: flex;
 	flex-direction: column;
-	padding: 20px 24px 20px 24px;
+	padding: 1.25rem 1.5rem 1.25rem 1.5rem;
 
-	border-radius: 12px;
+	border-radius: 0.75rem;
 	background: var(--GRAY-900, #181818);
 `;
 
@@ -30,8 +30,8 @@ export const TitleDiv = styled.div`
 	flex-direction: row;
 	align-items: center;
 
-	margin-bottom: 9.5px;
-	margin-left: 8px;
+	margin-bottom: 0.5938rem;
+	margin-left: 0.5rem;
 `;
 
 export const TitleContentDiv = styled.div`
@@ -39,36 +39,36 @@ export const TitleContentDiv = styled.div`
 
 	/* text/Bold 18pt */
 	font-family: Freesentation;
-	font-size: 18px;
+	font-size: 1.125rem;
 	font-style: normal;
 	font-weight: 700;
 	line-height: 140%; /* 25.2px */
-	letter-spacing: 0.72px;
+	letter-spacing: 0.045rem;
 `;
 
 export const PriceDiv = styled.div`
 	display: flex;
 	flex-direction: row;
 	align-items: end;
-	margin-bottom: 8px;
+	margin-bottom: 0.5rem;
 `;
 export const NowPriceDiv = styled.div`
 	color: #fff;
 	font-family: Freesentation;
-	font-size: 20px;
+	font-size: 1.25rem;
 	font-style: normal;
 	font-weight: 700;
 	line-height: 100%; /* 20px */
-	letter-spacing: 0.4px;
+	letter-spacing: 0.025rem;
 `;
 export const AllPriceDiv = styled.div`
 	color: #fff;
 	font-family: Freesentation;
-	font-size: 12px;
+	font-size: 0.75rem;
 	font-style: normal;
 	font-weight: 400;
 	line-height: 100%; /* 12px */
-	letter-spacing: 0.24px;
+	letter-spacing: 0.015rem;
 `;
 
 export const TextDiv = styled.div`
@@ -76,9 +76,32 @@ export const TextDiv = styled.div`
 
 	/* text/Regular 14pt */
 	font-family: Freesentation;
-	font-size: 14px;
+	font-size: 0.875rem;
 	font-style: normal;
 	font-weight: 400;
 	line-height: 140%; /* 19.6px */
 	letter-spacing: 0.56px;
+`;
+
+export const ColumnFlexDiv = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+export const RowFlexDiv = styled.div`
+	display: flex;
+	flex-direction: row;
+`;
+
+export const ImgDiv = styled.div`
+	position: absolute;
+	top: 0.75rem;
+	right: 0rem;
+`;
+
+export const GrayRoundDiv = styled.div`
+	border-radius: 12px;
+	background: var(--Foundation-Gray-gray700, #303036);
+
+	padding: 0.875rem;
+	margin: 0.75rem;
 `;
