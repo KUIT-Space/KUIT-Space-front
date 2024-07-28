8274cbe4e3351db933d7f31df209dafcd57d576c
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-27 21:48:45 -0700

    feat : adding LiveKit API

--- src/pages/VoiceRoomPage/VoiceRoomPage.tsx
+++ src/pages/VoiceRoomPage/VoiceRoomPage.tsx
@@ -12,30 +12,56 @@ import { useNavigate } from "react-router-dom";
 import * as s from "@/pages/VoiceRoomPage/VoiceRoomPage.styled";
 import * as sty from "@/components/TopBarText.styled";
 
+import { ControlBar, GridLayout, LiveKitRoom, ParticipantTile, RoomAudioRenderer, useTracks } from "@livekit/components-react";
+import "@livekit/components-styles";
+import { Track } from "livekit-client";
+
+const TopBarImage = ({ VoiceRoomName, setJoin }: { VoiceRoomName: string; setJoin: React.Dispatch<React.SetStateAction<boolean>> }) => {
+	return (
+		<div>
+			<sty.StyledTopBarDiv>
+				<sty.StyledLeftDiv
+					onClick={() => {
+						setJoin(false);
+					}}
+				>
+					<img src={back}></img>
+				</sty.StyledLeftDiv>
+				<sty.StyledCenterDiv>
+					<sty.StyledCenterP>{VoiceRoomName}</sty.StyledCenterP>
+				</sty.StyledCenterDiv>
+				<sty.StyledRightDiv>
+					<img src={setting} />
+				</sty.StyledRightDiv>
+			</sty.StyledTopBarDiv>
+		</div>
+	);
+};
 const VoiceRoomPage = ({ VoiceRoomName, setJoin }: { VoiceRoomName: string; setJoin: React.Dispatch<React.SetStateAction<boolean>> }) => {
 	const navigate = useNavigate();
+
+	const [token, setToken] = useState("");
+
 	return (
 		<>
-			<div>
-				<sty.StyledTopBarDiv>
-					<sty.StyledLeftDiv
-						onClick={() => {
-							setJoin(false);
-						}}
-					>
-						<img src={back}></img>
-					</sty.StyledLeftDiv>
-					<sty.StyledCenterDiv>
-						<sty.StyledCenterP>{VoiceRoomName}</sty.StyledCenterP>
-					</sty.StyledCenterDiv>
-					<sty.StyledRightDiv>
-						<img src={setting} />
-					</sty.StyledRightDiv>
-				</sty.StyledTopBarDiv>
-			</div>
+			<TopBarImage VoiceRoomName={VoiceRoomName} setJoin={setJoin} />
 			<s.ContentDiv>
 				<div>
-					<canvas></canvas>
+					<LiveKitRoom
+						video={true}
+						audio={true}
+						token={token}
+						serverUrl={process.env.NEXT_PUBLIC_LK_SERVER_URL}
+						// Use the default LiveKit theme for nice styles.
+						data-lk-theme="default"
+						style={{ height: "100vh" }}
+					>
+						{/* The RoomAudioRenderer takes care of room-wide audio for you. */}
+						<RoomAudioRenderer />
+						{/* Controls for the user to start/stop audio, video, and screen
+      share tracks and to leave the room. */}
+						<ControlBar />
+					</LiveKitRoom>
 				</div>
 				<s.BottomDiv>
 					<div>
