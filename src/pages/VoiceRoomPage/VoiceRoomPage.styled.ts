7da686e8b63e623a33127ad173bfb77ab77cb228
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-27 20:15:00 -0700

    Create VoiceRoomPage.styled.ts

---
+++ src/pages/VoiceRoomPage/VoiceRoomPage.styled.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const BottomDiv = styled.div`
+	display: flex;
+	justify-content: space-between;
+	text-align: center;
+	width: 50%;
+	margin: 0rem 3.25rem 0.75rem 3.25rem;
+	flex-direction: row;
+
+	position: fixed;
+	bottom: 0;
+`;
+
+export const ContentDiv = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+`;
