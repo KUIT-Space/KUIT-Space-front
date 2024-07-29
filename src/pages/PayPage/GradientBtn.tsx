4c2e6b0078a979936e1da4747a63e39176f46fd3
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-07-28 20:08:36 -0700

    feat: 버튼 컴포넌트 생성

--- src/pages/PayPage/GradientBtn.tsx
+++ src/pages/PayPage/GradientBtn.tsx
@@ -1,22 +1,22 @@
 import styled from "styled-components";
 import React from "react";
 
-export const NormalBtn = styled.button`
-	margin: 1rem;
-	padding: 0.75rem 0rem 0.75rem 0rem;
+export const GradientBtn = styled.button`
+	margin: 0.75rem;
+	padding: 0.875rem 0rem 0.8125rem 0rem;
 
 	border: none;
-	border-radius: 12px;
-	background: var(--Foundation-Main-color-Normal, #48ffbd);
+	border-radius: 0.75rem;
+	background: var(--Foundation-Main-color-Sub-color-gradient02, linear-gradient(76deg, #5ad7fe -1.06%, #48ffbd 96.33%));
 
 	color: var(--Foundation-Gray-gray900_background, #171719);
 	text-align: center;
 	font-family: Freesentation;
-	font-size: 1.125rem;
+	font-size: 18px;
 	font-style: normal;
 	font-weight: 600;
-	line-height: 140%; /* 1.575rem */
-	letter-spacing: 0.0225rem;
+	line-height: 140%; /* 25.2px */
+	letter-spacing: 0.36px;
 
 	/* background: ${(props) => (props.disabled ? "var(--Foundation-Gray-gray600, #45454B);" : "var(--Foundation-Main-color-Normal, #48FFBD)")};
 	color: ${(props) => (props.disabled ? "var(--Foundation-Gray-gray400, var(--GRAY-400, #ACACB5));" : "var(--Foundation-Gray-gray900_background, #171719)")}; */
--- src/pages/PayPage/GrayBtn.tsx
+++ src/pages/PayPage/GrayBtn.tsx
@@ -1,22 +1,22 @@
 import styled from "styled-components";
 import React from "react";
 
-export const NormalBtn = styled.button`
+export const GrayBtn = styled.button`
 	margin: 1rem;
 	padding: 0.75rem 0rem 0.75rem 0rem;
 
 	border: none;
 	border-radius: 12px;
-	background: var(--Foundation-Main-color-Normal, #48ffbd);
+	background: var(--Foundation-Gray-gray600, #45454b);
 
-	color: var(--Foundation-Gray-gray900_background, #171719);
+	color: var(--Foundation-Gray-gray500, #767681);
 	text-align: center;
 	font-family: Freesentation;
-	font-size: 1.125rem;
+	font-size: 18px;
 	font-style: normal;
 	font-weight: 600;
-	line-height: 140%; /* 1.575rem */
-	letter-spacing: 0.0225rem;
+	line-height: 140%; /* 25.2px */
+	letter-spacing: 0.36px;
 
 	/* background: ${(props) => (props.disabled ? "var(--Foundation-Gray-gray600, #45454B);" : "var(--Foundation-Main-color-Normal, #48FFBD)")};
 	color: ${(props) => (props.disabled ? "var(--Foundation-Gray-gray400, var(--GRAY-400, #ACACB5));" : "var(--Foundation-Gray-gray900_background, #171719)")}; */
