bcdee88d3534a702ef02bd536ba009c91aa3de44
김지환 (Jihwan Kim) <kjhwan0802@naver.com>
2024-07-29 23:43:08 +0900

    style: useSpaces

--- src/pages/ChatPage/ChatCreatePage/ChatCreatePage.styled.ts
+++ src/pages/ChatPage/ChatCreatePage/ChatCreatePage.styled.ts
@@ -4,124 +4,129 @@ import SearchIcon from "@/assets/ChatPage/icon_search.svg";
 import { BottomBtn } from "@/components/BottomBtn";
 
 export const ChatroomAddImgBtn = styled.label<{ $backgroundImage: string | null | undefined }>`
-	display: flex;
-	width: 8.5rem;
-	height: 8.5rem;
-	padding: 2.25rem;
-	margin: 2.5rem auto;
-	justify-content: center;
-	align-items: center;
-	flex-shrink: 0;
-	cursor: pointer;
+  display: flex;
+  width: 8.5rem;
+  height: 8.5rem;
+  padding: 2.25rem;
+  margin: 2.5rem auto;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  cursor: pointer;
 
-	border-radius: 0.75rem;
-	background: ${(props) => (props.$backgroundImage ? `url(${props.$backgroundImage}) no-repeat center` : "var(--Foundation-Gray-gray500, #767681)")};
-	background-size: cover;
+  border-radius: 0.75rem;
+  background: ${(props) =>
+    props.$backgroundImage
+      ? `url(${props.$backgroundImage}) no-repeat center`
+      : "var(--Foundation-Gray-gray500, #767681)"};
+  background-size: cover;
 `;
 
 export const ChatCreateContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 2rem;
-	margin: 0 1.25rem 6rem 1.25rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  margin: 0 1.25rem 6rem 1.25rem;
 
-	.input--container {
-		display: flex;
-		flex-direction: column;
-		gap: 0.5rem;
-	}
+  .input--container {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
 
-	.invite {
-		margin-right: 0.5rem;
-	}
+  .invite {
+    margin-right: 0.5rem;
+  }
 
-	.member--number {
-		color: ${({ theme }) => theme.colors.normal};
-	}
+  .member--number {
+    color: ${({ theme }) => theme.colors.normal};
+  }
 `;
 
 export const InviteInput = styled(Input)`
-	background-position: 1rem;
-	background-image: url(${SearchIcon});
-	background-repeat: no-repeat;
-	padding-left: 2.5rem;
+  background-position: 1rem;
+  background-image: url(${SearchIcon});
+  background-repeat: no-repeat;
+  padding-left: 2.5rem;
 `;
 
 export const ChatroomName = styled.div<{ $nameLength: number }>`
-	position: relative;
+  position: relative;
 
-	span {
-		position: absolute;
-		bottom: 0.9375rem;
-		right: 1rem;
+  span {
+    position: absolute;
+    bottom: 0.9375rem;
+    right: 1rem;
 
-		color: var(--Foundation-Gray-gray500, #767681);
-		text-align: right;
+    color: var(--Foundation-Gray-gray500, #767681);
+    text-align: right;
 
-		/* text/Regular 16pt */
-		font-family: Freesentation;
-		font-size: 1rem;
-		font-style: normal;
-		font-weight: 400;
-		line-height: 140%; /* 1.4rem */
-		letter-spacing: 0.04rem;
-	}
+    /* text/Regular 16pt */
+    font-family: Freesentation;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 140%; /* 1.4rem */
+    letter-spacing: 0.04rem;
+  }
 
-	input {
-		caret-color: ${(props) => (props.$nameLength >= 15 ? props.theme.colors.char_red : props.theme.colors.normal)};
-		&:focus {
-			border-color: ${(props) => (props.$nameLength >= 15 ? props.theme.colors.char_red : props.theme.colors.normal)};
-		}
-	}
+  input {
+    caret-color: ${(props) =>
+      props.$nameLength >= 15 ? props.theme.colors.char_red : props.theme.colors.normal};
+    &:focus {
+      border-color: ${(props) =>
+        props.$nameLength >= 15 ? props.theme.colors.char_red : props.theme.colors.normal};
+    }
+  }
 `;
 
 export const Member = styled.label`
-	display: flex;
-	user-select: none;
-	padding: 0.5rem 0rem;
-	align-items: center;
-	justify-content: space-between;
-	cursor: pointer;
+  display: flex;
+  user-select: none;
+  padding: 0.5rem 0rem;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
 
-	section {
-		display: flex;
-		align-items: center;
-	}
+  section {
+    display: flex;
+    align-items: center;
+  }
 
-	.name {
-		padding-left: 1rem;
-		color: var(--Foundation-Gray-white, #fff);
+  .name {
+    padding-left: 1rem;
+    color: var(--Foundation-Gray-white, #fff);
 
-		/* text/Regular 16pt */
-		font-family: Freesentation;
-		font-size: 1rem;
-		font-style: normal;
-		font-weight: 400;
-		line-height: 140%; /* 1.4rem */
-		letter-spacing: 0.04rem;
-	}
+    /* text/Regular 16pt */
+    font-family: Freesentation;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 140%; /* 1.4rem */
+    letter-spacing: 0.04rem;
+  }
 
-	.admin {
-		padding-left: 0.5rem;
-		color: var(--Foundation-Main-color-Normal, var(--normal, #48ffbd));
-		text-align: center;
+  .admin {
+    padding-left: 0.5rem;
+    color: var(--Foundation-Main-color-Normal, var(--normal, #48ffbd));
+    text-align: center;
 
-		/* text/Regular 14pt */
-		font-family: Freesentation;
-		font-size: 0.875rem;
-		font-style: normal;
-		font-weight: 400;
-		line-height: 140%; /* 1.225rem */
-		letter-spacing: 0.035rem;
-	}
+    /* text/Regular 14pt */
+    font-family: Freesentation;
+    font-size: 0.875rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 140%; /* 1.225rem */
+    letter-spacing: 0.035rem;
+  }
 
-	img {
-		width: 2.5rem;
-		height: 2.5rem;
-		border-radius: 6.25rem;
-	}
+  img {
+    width: 2.5rem;
+    height: 2.5rem;
+    border-radius: 6.25rem;
+  }
 `;
 
 export const ChatCreateBottomBtn = styled(BottomBtn)`
-	margin: 0;
+  margin: 0;
 `;
--- src/pages/ChatPage/ChatPage.tsx
+++ src/pages/ChatPage/ChatPage.tsx
@@ -5,65 +5,128 @@ import { ChatListContainer, ChatContainer } from "./ChatPage.styled";
 import { AddChatBtn } from "./ChatAddBtn.styled";
 
 const ChatPage = () => {
-	const navigate = useNavigate();
-	const [isAdmin, setIsAdmin] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
 
-	useEffect(() => {
-		//admin 확인 부분
-		setIsAdmin(true);
-	}, []);
+  useEffect(() => {
+    //admin 확인 부분
+    setIsAdmin(true);
+  }, []);
 
-	const tempJson = {
-		chatList: [
-			{ id: 1, image: "https://placehold.co/40x40", title: "작죽디수다방", time: "오늘 오후 4:15", detail: "채팅 내용은 이렇습니다.", chat_num: "999" },
-			{ id: 2, image: "https://placehold.co/40x40", title: "작죽디수다방", time: "오늘 오후 4:15", detail: "채팅 내용은 이렇습니다.", chat_num: "999" },
-			{ id: 3, image: "https://placehold.co/40x40", title: "작죽디수다방", time: "오늘 오후 4:15", detail: "채팅 내용은 이렇습니다.", chat_num: "999" },
-			{ id: 4, image: "https://placehold.co/40x40", title: "작죽디수다방", time: "오늘 오후 4:15", detail: "채팅 내용은 이렇습니다.", chat_num: "999" },
-			{ id: 5, image: "https://placehold.co/40x40", title: "작죽디수다방", time: "오늘 오후 4:15", detail: "채팅 내용은 이렇습니다.", chat_num: "999" },
-			{ id: 6, image: "https://placehold.co/40x40", title: "작죽디수다방", time: "오늘 오후 4:15", detail: "채팅 내용은 이렇습니다.", chat_num: "999" },
-			{ id: 7, image: "https://placehold.co/40x40", title: "작죽디수다방", time: "오늘 오후 4:15", detail: "채팅 내용은 이렇습니다.", chat_num: "999" },
-			{ id: 8, image: "https://placehold.co/40x40", title: "작죽디수다방", time: "오늘 오후 4:15", detail: "채팅 내용은 이렇습니다.", chat_num: "999" },
-			{ id: 9, image: "https://placehold.co/40x40", title: "작죽디수다방", time: "오늘 오후 4:15", detail: "채팅 내용은 이렇습니다.", chat_num: "999" },
-		],
-	};
+  const tempJson = {
+    chatList: [
+      {
+        id: 1,
+        image: "https://placehold.co/40x40",
+        title: "작죽디수다방",
+        time: "오늘 오후 4:15",
+        detail: "채팅 내용은 이렇습니다.",
+        chat_num: "999",
+      },
+      {
+        id: 2,
+        image: "https://placehold.co/40x40",
+        title: "작죽디수다방",
+        time: "오늘 오후 4:15",
+        detail: "채팅 내용은 이렇습니다.",
+        chat_num: "999",
+      },
+      {
+        id: 3,
+        image: "https://placehold.co/40x40",
+        title: "작죽디수다방",
+        time: "오늘 오후 4:15",
+        detail: "채팅 내용은 이렇습니다.",
+        chat_num: "999",
+      },
+      {
+        id: 4,
+        image: "https://placehold.co/40x40",
+        title: "작죽디수다방",
+        time: "오늘 오후 4:15",
+        detail: "채팅 내용은 이렇습니다.",
+        chat_num: "999",
+      },
+      {
+        id: 5,
+        image: "https://placehold.co/40x40",
+        title: "작죽디수다방",
+        time: "오늘 오후 4:15",
+        detail: "채팅 내용은 이렇습니다.",
+        chat_num: "999",
+      },
+      {
+        id: 6,
+        image: "https://placehold.co/40x40",
+        title: "작죽디수다방",
+        time: "오늘 오후 4:15",
+        detail: "채팅 내용은 이렇습니다.",
+        chat_num: "999",
+      },
+      {
+        id: 7,
+        image: "https://placehold.co/40x40",
+        title: "작죽디수다방",
+        time: "오늘 오후 4:15",
+        detail: "채팅 내용은 이렇습니다.",
+        chat_num: "999",
+      },
+      {
+        id: 8,
+        image: "https://placehold.co/40x40",
+        title: "작죽디수다방",
+        time: "오늘 오후 4:15",
+        detail: "채팅 내용은 이렇습니다.",
+        chat_num: "999",
+      },
+      {
+        id: 9,
+        image: "https://placehold.co/40x40",
+        title: "작죽디수다방",
+        time: "오늘 오후 4:15",
+        detail: "채팅 내용은 이렇습니다.",
+        chat_num: "999",
+      },
+    ],
+  };
 
-	return (
-		<>
-			<TopBarText left={LeftEnum.Logo} center="채팅방" right=""></TopBarText>
-			<ChatListContainer>
-				{tempJson.chatList.map((chat) => {
-					return (
-						<ChatContainer
-							key={chat.id}
-							onClick={() => {
-								navigate(`/chat/${chat.id}`);
-							}}
-						>
-							<img className="chat-btn-img" alt="chat-btn-img" src={chat.image} />
-							<div className="chat--container">
-								<div className="chat-btn--title-time--container">
-									<div className="chat-btn-title">{chat.title}</div>
-									<div className="chat-btn-time">{chat.time}</div>
-								</div>
-								<div className="chat-btn--detail-chatNum--container">
-									<div className="chat-btn-detail">{chat.detail}</div>
-									<div className="chat-btn-chatNum">{chat.chat_num}</div>
-								</div>
-							</div>
-						</ChatContainer>
-					);
-				})}
-			</ChatListContainer>
+  return (
+    <>
+      <TopBarText left={LeftEnum.Logo} center="채팅방" right=""></TopBarText>
+      <ChatListContainer>
+        {tempJson.chatList.map((chat) => {
+          return (
+            <ChatContainer
+              key={chat.id}
+              onClick={() => {
+                navigate(`/chat/${chat.id}`);
+              }}
+            >
+              <img className="chat-btn-img" alt="chat-btn-img" src={chat.image} />
+              <div className="chat--container">
+                <div className="chat-btn--title-time--container">
+                  <div className="chat-btn-title">{chat.title}</div>
+                  <div className="chat-btn-time">{chat.time}</div>
+                </div>
+                <div className="chat-btn--detail-chatNum--container">
+                  <div className="chat-btn-detail">{chat.detail}</div>
+                  <div className="chat-btn-chatNum">{chat.chat_num}</div>
+                </div>
+              </div>
+            </ChatContainer>
+          );
+        })}
+      </ChatListContainer>
 
-			{isAdmin && (
-				<AddChatBtn
-					onClick={() => {
-						navigate(`/chat/create`);
-					}}
-				/>
-			)}
-		</>
-	);
+      {isAdmin && (
+        <AddChatBtn
+          onClick={() => {
+            navigate(`/chat/create`);
+          }}
+        />
+      )}
+    </>
+  );
 };
 
 export default ChatPage;
