623c2b8b2e90d65990c72d200df20ce189611e89
YangJJune <58018839+YangJJune@users.noreply.github.com>
2024-08-01 01:00:25 -0700

    feat : /requestingpay 에 파라미터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { createBrowserRouter, matchPath, Outlet, RouterProvider, useLocation } from "react-router-dom";
+import {
+  createBrowserRouter,
+  matchPath,
+  Outlet,
+  RouterProvider,
+  useLocation,
+} from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
 import { theme } from "@/styles/Theme";
 import GlobalStyle from "@/styles/GlobalStyles";
@@ -34,67 +40,70 @@ import CompletePay from "./pages/PayPage/CompletePay";
 // };
 
 const LayoutContainer = styled.div`
-	position: relative;
-	min-width: 360px;
-	max-width: 720px;
-	width: 100%;
-	margin: 0 auto;
+  position: relative;
+  min-width: 360px;
+  max-width: 720px;
+  width: 100%;
+  margin: 0 auto;
 
-	#content {
-		min-height: calc(100vh - 3.75rem);
-	}
+  #content {
+    min-height: calc(100vh - 3.75rem);
+  }
 `;
 
 interface RouteChildren {
-	path: string;
-	element: JSX.Element;
-	hasBottomBar?: boolean;
+  path: string;
+  element: JSX.Element;
+  hasBottomBar?: boolean;
 }
 
 function Layout({ routes_children }: { routes_children: RouteChildren[] }) {
-	const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-	return (
-		<ThemeProvider theme={theme}>
-			<GlobalStyle />
-			<LayoutContainer>
-				<div id="content">
-					<Outlet />
-				</div>
-				{routes_children.find((child) => matchPath(child.path, pathname))?.hasBottomBar && <BottomNavBar />}
-			</LayoutContainer>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <LayoutContainer>
+        <div id="content">
+          <Outlet />
+        </div>
+        {routes_children.find((child) => matchPath(child.path, pathname))?.hasBottomBar && (
+          <BottomNavBar />
+        )}
+      </LayoutContainer>
+    </ThemeProvider>
+  );
 }
 
 function App() {
-	const routes_children = [
-		{ path: "/", element: <HomePage />, hasBottomBar: true },
-		{ path: "/chat", element: <ChatPage />, hasBottomBar: true },
-		{ path: "/chat/:id", element: <ChattingPage /> },
-		{ path: "/chat/create", element: <ChatCreatePage /> },
-		{ path: "/pay", element: <PayPage />, hasBottomBar: true },
-		{ path: "/requestingpay", element: <MyRequestPayPage />, hasBottomBar: false },
-		{ path: "/requestedpay", element: <RequestedPayPage />, hasBottomBar: false },
-		{ path: "/completepay", element: <CompletePay />, hasBottomBar: false },
-		{ path: "/board", element: <BoardPage />, hasBottomBar: true },
-		{ path: "/voiceroom", element: <VoiceRoomListPage />, hasBottomBar: true },
-		{ path: "/createvoiceroom", element: <CreateVoiceRoomPage />, hasBottomBar: false },
-		{ path: "/joinvoiceroom", element: <JoinVoiceRoomPage />, hasBottombar: false },
-		{ path: "/editvoiceroom", element: <EditVoiceRoomPage />, hasBottombar: false },
-	];
+  const routes_children = [
+    { path: "/", element: <HomePage />, hasBottomBar: true },
+    { path: "/chat", element: <ChatPage />, hasBottomBar: true },
+    { path: "/chat/:id", element: <ChattingPage /> },
+    { path: "/chat/create", element: <ChatCreatePage /> },
+    { path: "/pay", element: <PayPage />, hasBottomBar: true },
+    { path: "/requestingpay", element: <MyRequestPayPage />, hasBottomBar: false },
+    { path: "/requestingpay/:id", element: <MyRequestPayPage />, hasBottomBar: false },
+    { path: "/requestedpay", element: <RequestedPayPage />, hasBottomBar: false },
+    { path: "/completepay", element: <CompletePay />, hasBottomBar: false },
+    { path: "/board", element: <BoardPage />, hasBottomBar: true },
+    { path: "/voiceroom", element: <VoiceRoomListPage />, hasBottomBar: true },
+    { path: "/createvoiceroom", element: <CreateVoiceRoomPage />, hasBottomBar: false },
+    { path: "/joinvoiceroom", element: <JoinVoiceRoomPage />, hasBottombar: false },
+    { path: "/editvoiceroom", element: <EditVoiceRoomPage />, hasBottombar: false },
+  ];
 
-	const routes = [
-		{ path: "/login", element: <LoginPage /> },
-		{ path: "/signUp", element: <SignUpPage /> },
-		{
-			element: <Layout routes_children={routes_children} />,
-			children: routes_children,
-		},
-	];
-	const router = createBrowserRouter(routes);
+  const routes = [
+    { path: "/login", element: <LoginPage /> },
+    { path: "/signUp", element: <SignUpPage /> },
+    {
+      element: <Layout routes_children={routes_children} />,
+      children: routes_children,
+    },
+  ];
+  const router = createBrowserRouter(routes);
 
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/PayPage/MyRequestPayPage.tsx
+++ src/pages/PayPage/MyRequestPayPage.tsx
@@ -3,25 +3,30 @@ import * as s from "@/pages/PayPage/PayPage.styled";
 import { useState } from "react";
 import MyReqDataDiv from "./MyReqDataDiv";
 import GrayMyReqDataDiv from "./GrayMyReqDataDiv";
+import { useParams } from "react-router-dom";
 
 const MyRequestPayPage = () => {
-	const [index, setIndex] = useState(-1);
-
-	return (
-		<>
-			<TopBarText left={LeftEnum.Back} center="내가 요청한 정산" right=""></TopBarText>
-			<s.ContainerDiv>
-				<div>
-					<s.TitleContentDiv>진행 중인 정산</s.TitleContentDiv>
-					<MyReqDataDiv></MyReqDataDiv>
-				</div>
-				<div style={{ marginTop: "2.75rem" }}>
-					<s.TitleContentDiv>완료된 정산</s.TitleContentDiv>
-					<GrayMyReqDataDiv></GrayMyReqDataDiv>
-				</div>
-			</s.ContainerDiv>
-		</>
-	);
+  const [index, setIndex] = useState(-1);
+  let { id } = useParams();
+  return (
+    <>
+      <TopBarText left={LeftEnum.Back} center="내가 요청한 정산" right=""></TopBarText>
+      {typeof id === "undefined" ? (
+        <s.ContainerDiv>
+          <div>
+            <s.TitleContentDiv>진행 중인 정산</s.TitleContentDiv>
+            <MyReqDataDiv></MyReqDataDiv>
+          </div>
+          <div style={{ marginTop: "2.75rem" }}>
+            <s.TitleContentDiv>완료된 정산</s.TitleContentDiv>
+            <GrayMyReqDataDiv></GrayMyReqDataDiv>
+          </div>
+        </s.ContainerDiv>
+      ) : (
+        <div>{id}</div>
+      )}
+    </>
+  );
 };
 
 export default MyRequestPayPage;
