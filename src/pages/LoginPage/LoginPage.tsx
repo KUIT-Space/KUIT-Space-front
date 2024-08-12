250e15d4da34f3836ef8a5e90ed7a8e35fc124e0
김지환 (Jihwan Kim) <kjhwan0802@naver.com>
2024-08-13 01:18:27 +0900

    api: login page에 api 연결

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,50 +1,78 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Container, Logo, Input, LoginButton, BtContainer, Button, ScContainer, Social } from "@/pages/LoginPage/LoginPage.styled.ts";
-import logoSpace from "@/assets/logo_space.svg";
-import kakao from "@/assets/Login/icon_kakao.svg";
+
+import { loginApi } from "@/apis";
 import google from "@/assets/Login/icon_google.svg";
+import kakao from "@/assets/Login/icon_kakao.svg";
 import naver from "@/assets/Login/icon_naver.svg";
+import logoSpace from "@/assets/logo_space.svg";
+import {
+  BtContainer,
+  Button,
+  Container,
+  Input,
+  LoginButton,
+  Logo,
+  ScContainer,
+  Social,
+} from "@/pages/LoginPage/LoginPage.styled.ts";
 
 const LoginPage = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const [id, setId] = useState("");
-	const [password, setPassword] = useState("");
-	const [isButtonActive, setIsButtonActive] = useState(false);
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [isButtonActive, setIsButtonActive] = useState(false);
 
-	useEffect(() => {
-		setIsButtonActive(id.trim() !== "" && password.trim() !== "");
-	}, [id, password]);
+  useEffect(() => {
+    setIsButtonActive(id.trim() !== "" && password.trim() !== "");
+  }, [id, password]);
 
-	return (
-		<>
-			<Container>
-				<Logo>
-					<img src={logoSpace} style={{ width: "100%" }} alt="Logo" />
-				</Logo>
-				<Input type="id" placeholder="아이디" value={id} onChange={(e) => setId(e.target.value)} style={{ marginTop: "10.37rem" }} />
-				<Input type="password" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} />
-				<LoginButton $isActive={isButtonActive}>로그인</LoginButton>
-				<BtContainer>
-					<Button>아이디 찾기</Button>
-					<Button>비밀번호 찾기</Button>
-					<Button onClick={() => navigate("/signup")}>회원가입</Button>
-				</BtContainer>
-				<ScContainer>
-					<Social>
-						<img src={kakao} alt="kakao" />
-					</Social>
-					<Social>
-						<img src={google} alt="google" />
-					</Social>
-					<Social>
-						<img src={naver} alt="naver" />
-					</Social>
-				</ScContainer>
-			</Container>
-		</>
-	);
+  const handleLogin = () => {
+    loginApi(id, password);
+  };
+
+  return (
+    <>
+      <Container>
+        <Logo>
+          <img src={logoSpace} style={{ width: "100%" }} alt="Logo" />
+        </Logo>
+        <Input
+          type="id"
+          placeholder="아이디"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          style={{ marginTop: "10.37rem" }}
+        />
+        <Input
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <LoginButton onClick={handleLogin} $isActive={isButtonActive}>
+          로그인
+        </LoginButton>
+        <BtContainer>
+          <Button>아이디 찾기</Button>
+          <Button>비밀번호 찾기</Button>
+          <Button onClick={() => navigate("/signup")}>회원가입</Button>
+        </BtContainer>
+        <ScContainer>
+          <Social>
+            <img src={kakao} alt="kakao" />
+          </Social>
+          <Social>
+            <img src={google} alt="google" />
+          </Social>
+          <Social>
+            <img src={naver} alt="naver" />
+          </Social>
+        </ScContainer>
+      </Container>
+    </>
+  );
 };
 
 export default LoginPage;
