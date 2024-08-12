671c9649104a2a1b083beb5cfd3ffd43ed5e5a88
김지환 (Jihwan Kim) <kjhwan0802@naver.com>
2024-08-13 01:18:13 +0900

    feat: add login api ,api 모듈화 분리

---
+++ src/apis/LoginApi.ts
@@ -0,0 +1,28 @@
+import { createRequestOptionsJSON, RequestOptions } from "./_createRequestOptions";
+
+const fetchLoginApi = async (url: string, options: RequestOptions) => {
+  const response = await fetch(url, options)
+    .then((res) => res.text())
+    .catch((err) => console.error(err));
+
+  //console.log(response);
+
+  return response;
+};
+
+export const loginApi = async (email: string, password: string) => {
+  const body = {
+    email: email,
+    password: password,
+  };
+
+  const requestOptions = createRequestOptionsJSON("POST", JSON.stringify(body));
+  const response = await fetchLoginApi("/api/user/login", requestOptions);
+
+  // Authorization token 응답에 포함되면 local storage에 저장
+  if (response && response.token) {
+    localStorage.setItem("Authorization", response.token);
+  }
+
+  return response;
+};
