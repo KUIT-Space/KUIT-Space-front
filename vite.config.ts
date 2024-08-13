3a99fd68ad129b04e613e478d4a3b5a3f4e9171b
YangJJune <ysj7191@naver.com>
2024-08-14 07:38:18 +0900

    https 지워져서 다시

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
 import { defineConfig } from "vite";
 import vitePluginSvgr from "vite-plugin-svgr";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginSvgr() ],
+  plugins: [react(), vitePluginSvgr(), mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -17,18 +18,18 @@ export default defineConfig({
     },
   },
   base: "/KUIT-Space-front/",
-  server:{
-    proxy:{
-      '/api': {
-        target: 'https://www.project-space.xyz/',
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://www.project-space.xyz/",
         changeOrigin: true,
         // 요청 경로에서 '/api' 제거
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
         // SSL 인증서 검증 무시
         secure: false,
         // WebSocket 프로토콜 사용
         ws: true,
       },
-    }
-  }
+    },
+  },
 });
