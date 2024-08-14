061245c9fa5d6bde90d0b305e8d84b063f9f083f
김지환 (Jihwan Kim) <kjhwan0802@naver.com>
2024-08-15 01:21:20 +0900

    fix: prevent double JSON stringify

--- src/apis/_createRequestOptions.ts
+++ src/apis/_createRequestOptions.ts
@@ -11,7 +11,7 @@ export const createRequestOptionsJSON = (
 ): RequestOptions => {
   return {
     method: method,
-    body: JSON.stringify(body),
+    body: body,
     redirect: "follow",
     headers: {
       "Content-Type": "application/json",
@@ -39,7 +39,7 @@ export const createRequestOptionsJSON_AUTH = (
   return token
     ? {
         method: method,
-        body: JSON.stringify(body),
+        body: body,
         redirect: "follow",
         headers: {
           "Content-Type": "application/json",
