93c43b0ad62f437f0133c29b4ef9782be9ca7ea5
YangJJune <ysj7191@naver.com>
2024-08-17 14:13:54 +0900

    feat : 정산이 없는 경우 VIew 추가

--- src/pages/PayPage/MyRequestPayPage.tsx
+++ src/pages/PayPage/MyRequestPayPage.tsx
@@ -33,19 +33,34 @@ const MyRequestPayPage = () => {
   return (
     <>
       <TopBarText left={LeftEnum.Back} center="내가 요청한 정산" right=""></TopBarText>
+
       {typeof id === "undefined" ? (
         <s.ContainerDiv>
           <div>
             <s.TitleContentDiv>진행 중인 정산</s.TitleContentDiv>
-            {currentData?.map((value) => {
-              return <MyReqDataDiv key={value.payRequestId} data={value}></MyReqDataDiv>;
-            })}
+            {currentData?.length == 0 ? (
+              <s.NoAlertDiv>요청한 정산이 없어요!</s.NoAlertDiv>
+            ) : (
+              <div>
+                {currentData?.map((value) => {
+                  return <MyReqDataDiv key={value.payRequestId} data={value}></MyReqDataDiv>;
+                })}
+              </div>
+            )}
           </div>
           <div style={{ marginTop: "2.75rem" }}>
             <s.TitleContentDiv>완료된 정산</s.TitleContentDiv>
-            {completeData?.map((value) => {
-              return <GrayMyReqDataDiv key={value.payRequestId} data={value}></GrayMyReqDataDiv>;
-            })}
+            {completeData?.length == 0 ? (
+              <s.NoAlertDiv>요청한 정산이 없어요!</s.NoAlertDiv>
+            ) : (
+              <div>
+                {completeData?.map((value) => {
+                  return (
+                    <GrayMyReqDataDiv key={value.payRequestId} data={value}></GrayMyReqDataDiv>
+                  );
+                })}
+              </div>
+            )}
           </div>
         </s.ContainerDiv>
       ) : (
