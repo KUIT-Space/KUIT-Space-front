84fa1d42350fd7ae47609bd2fbf2a98066d7be82
Byeolchan Kim <qufstar46@gmail.com>
2025-02-02 02:08:15 +0900

    feat: footer stays right above keyboard ui

---
+++ src/components/KeyboardAccessoryView.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode, useCallback, useEffect, useLayoutEffect, useRef } from "react";
+import styled from "styled-components";
+
+export interface KeyboardAccessoryViewProps {
+  children: ReactNode;
+}
+
+const StyledKeyboardAccessoryView = styled.div`
+  position: absolute;
+  bottom: 100px;
+`;
+
+const KeyboardAccessoryView: React.FC<KeyboardAccessoryViewProps> = ({ children }) => {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const handleVisualViewportResize = (event: Event) => {
+    const visualViewportHeight = window.visualViewport?.height;
+    if (visualViewportHeight && divRef.current) {
+      divRef.current.style.height = `${visualViewportHeight - 30}px`;
+      window.scrollTo(0, 40);
+    }
+  };
+
+  useEffect(() => {
+    const resizeAbortController = new AbortController();
+
+    if (visualViewport) {
+      visualViewport.addEventListener("resize", handleVisualViewportResize, {
+        signal: resizeAbortController.signal,
+      });
+    }
+
+    return () => {
+      console.log("visualViewport unregistered");
+      resizeAbortController.abort();
+    };
+  }, []);
+
+  return <StyledKeyboardAccessoryView>{children}</StyledKeyboardAccessoryView>;
+};
+
+export default KeyboardAccessoryView;
