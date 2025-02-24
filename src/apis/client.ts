2ac76ecb61f5768591371496128b51a067d7cd18
Byeolchan Kim <qufstar46@gmail.com>
2025-02-25 02:04:30 +0900

    feat: create ky instance with base configuration

---
+++ src/apis/client.ts
@@ -0,0 +1,34 @@
+import ky from "ky";
+
+export interface ApiResponse<T = unknown> {
+  code: number;
+  message: string;
+  status: string;
+  timestamp?: string;
+  result?: T;
+}
+
+export const client = ky.create({
+  prefixUrl: import.meta.env.VITE_API_BACK_URL,
+  hooks: {
+    beforeRequest: [
+      (request) => {
+        const token = localStorage.getItem("Authorization");
+        if (token) {
+          request.headers.set("Authorization", token);
+        }
+      },
+    ],
+    afterResponse: [
+      async (request, options, response) => {
+        if (response.status === 401) {
+          localStorage.removeItem("Authorization");
+        }
+      },
+    ],
+  },
+  retry: {
+    limit: 2,
+    methods: ["get", "put", "head", "delete", "options", "trace"],
+  },
+});
