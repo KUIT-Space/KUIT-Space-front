fa95dc287b4822c54f743172d7d6fbb20756e536
Byeolchan Kim <qufstar46@gmail.com>
2025-02-28 15:23:13 +0900

    fix: modify JSDoc to specify the return type more clearly

--- src/apis/Space/SpaceJoinInfoApi.ts
+++ src/apis/Space/SpaceJoinInfoApi.ts
@@ -14,7 +14,7 @@ interface SpaceJoinInfoResponse extends ApiResponse {
 /**
  * 초대링크를 받은 유저에게 스페이스 정보를 보여주는 API
  * @param spaceId - 유저를 초대할 스페이스의 ID
- * @returns SpaceJoinInfo | null - 스페이스 정보 또는 이미 가입된 유저 메시지
+ * @returns SpaceJoinInfoResponse | null - 스페이스 정보 또는 이미 가입된 유저 메시지
  */
 export const SpaceJoinInfoApi = async (spaceId: number) => {
   try {
@@ -25,7 +25,7 @@ export const SpaceJoinInfoApi = async (spaceId: number) => {
       return null;
     }
 
-    return response.result;
+    return response;
   } catch (error) {
     console.error("[SpaceJoinInfoApi error]", error);
     return null;
