c1ca7d666cfc9a6c1ec8c6b9e60a83a9e103ef0d
Byeolchan Kim <qufstar46@gmail.com>
2025-03-11 01:34:09 +0900

    feat: Add QueryProvider and wrap App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { QueryProvider } from "./apis/query-provider";
 import App from "./App.tsx";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryProvider>
+      <App />
+    </QueryProvider>
   </React.StrictMode>,
 );
