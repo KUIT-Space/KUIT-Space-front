b9138c1168c3ae9979ad8de5c3c798eb5086b7a2
Byeolchan Kim <qufstar46@gmail.com>
2025-03-27 02:42:29 +0900

    feat: add skeleton detail page as a global fallback

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 
 import { QueryProvider } from "./apis/query-provider";
+import SkeletonDetailPage from "./components/SkeletonDetailPage";
 import App from "./App.tsx";
 
 import "./index.css";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryProvider>
-      <Suspense>
+      <Suspense fallback={<SkeletonDetailPage />}>
         <App />
       </Suspense>
     </QueryProvider>
