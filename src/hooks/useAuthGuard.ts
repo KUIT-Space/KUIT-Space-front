4683efecde0dcb009866d5e543396e3e4dbe03dd
Byeolchan Kim <qufstar46@gmail.com>
2025-03-28 01:05:36 +0900

    refactor: rename hook

--- src/hooks/useAuthGuard.ts
+++ src/hooks/useAuthGuard.ts
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import { useAuthQuery } from "../apis/oauth";
 
-export const useAuthRedirect = (options?: { redirectTo?: string }) => {
+export const useAuthGuard = (options?: { redirectTo?: string }) => {
   const navigate = useNavigate();
   const { data: isAuthenticated } = useAuthQuery();
   const redirectTo = options?.redirectTo || "/discordlogin";
