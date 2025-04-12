9bdce80b7d7e2e4a6874adf206b67f1861a5361a
Byeolchan Kim <qufstar46@gmail.com>
2025-04-13 02:42:38 +0900

    storybook: error boundary and suspense for fallbacks

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,14 +1,16 @@
 import { initialize, mswLoader } from "msw-storybook-addon";
 import { handlers } from "../src/mocks/handlers";
 import type { Preview } from "@storybook/react";
-import React from "react";
+import React, { Suspense } from "react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../src/styles/Theme";
 import GlobalStyle from "../src/styles/GlobalStyles";
 import "../src/index.css";
 import { withRouter } from "storybook-addon-remix-react-router";
 import { QueryProvider } from "../src/apis/query-provider";
-
+import GlobalErrorFallback from "../src/components/GlobalErrorFallback";
+import { ErrorBoundary } from "react-error-boundary";
+import SkeletonDetailPage from "../src/components/SkeletonDetailPage";
 // Initialize MSW
 initialize();
 
@@ -30,7 +32,11 @@ const preview: Preview = {
       <QueryProvider>
         <ThemeProvider theme={theme}>
           <GlobalStyle />
-          <Story />
+          <ErrorBoundary FallbackComponent={GlobalErrorFallback}>
+            <Suspense fallback={<SkeletonDetailPage />}>
+              <Story />
+            </Suspense>
+          </ErrorBoundary>
         </ThemeProvider>
       </QueryProvider>
     ),
